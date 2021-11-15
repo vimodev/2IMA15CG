bdc9c0e9835e9f7a80a021a6378a52629a1fc662
Vincent <mail@vincentmoonen.com>
2021-11-15 15:27:02 +0100

    JSON library can parse jsons

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,18 @@
 #include <stdio.h>
+#include <iostream>
+#include <json.h>
+#include <fstream>
+#include <string.h>
+
+using json = nlohmann::json;
+using namespace std;
+
+string INSTANCES_PATH = "../instances/";
 
 int main(int argc, char **argv) {
-    printf("Hello world!\n");
+    json j;
+    std::ifstream input_stream(INSTANCES_PATH + "reecn3382.instance.json");
+    input_stream >> j;
+    cout << j["id"] << endl;
     return 0;
 }
